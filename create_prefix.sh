1ca19290a1008411bc5e370ee15e4c47a83f0bf7
Benjamin Lorenz <lorenz@math.tu-berlin.de>
2023-02-16 10:21:39 +0100

    set arch and prefix

--- create_prefix.sh
+++ create_prefix.sh
@@ -2,12 +2,43 @@
 
 set -e
 
+usage() { echo "Usage: $0 [-a arch] [-j julia] [-p prefix]" 1>&2; exit 1; }
+
 basedir=$(dirname $0)
 prefix=$basedir/prefix
+julia=julia
+arch="x86_64"
+
+args=`getopt a:j:p:h $*`
+set -- $args
+while :; do
+   case "$1" in
+      -a)
+         arch="$2"
+         shift; shift
+         ;;
+      -j)
+         julia="$2"
+         shift; shift
+         ;;
+      -p)
+         prefix="$2"
+         shift; shift
+         ;;
+      -h)
+         usage; exit 1;
+         ;;
+      --)
+         shift; break
+         ;;
+   esac
+done
+
+prefix=$prefix.$arch
 
 cd $basedir
 
-julia prepare_prefix.jl
+$julia populate_prefix.jl "$prefix" "$arch"
 
 patch -d $prefix -p1 < pm_prefix_new.patch
 
--- populate_prefix.jl
+++ populate_prefix.jl
@@ -1,11 +1,15 @@
 using Pkg;
+using Base.BinaryPlatforms;
 Pkg.activate(; temp=true);
 Pkg.add(url="https://github.com/JuliaPackaging/JLLPrefixes.jl");
 
 using JLLPrefixes
 
-prefix=joinpath(dirname(@__FILE__),"prefix");
-artifact_paths = collect_artifact_paths(["polymake_jll"])
+prefix=ARGS[1]
+arch=ARGS[2]
+
+platform=Platform(arch, "macos")
+artifact_paths = collect_artifact_paths(["polymake_jll"]; platform=platform)
 deploy_artifact_paths(prefix, artifact_paths; strategy=:copy)
 
 println("For sources see:")
